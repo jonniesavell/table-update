bbb1057f3b089eec99fd38052b2ce75f4cc6fbec
jonnie savell <jonniesavell@mac.com>
2024-06-25 14:45:01 -0700

    remove dependency-factory

--- src/main/java/com/indigententerprises/App.java
+++ src/main/java/com/indigententerprises/App.java
@@ -2,6 +2,10 @@ package com.indigententerprises;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 /**
@@ -11,12 +15,17 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
  * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java Handler</a> for more information
  */
 public class App implements RequestHandler<Object, Object> {
+
     private final DynamoDbClient dynamoDbClient;
 
     public App() {
         // Initialize the SDK client outside of the handler method so that it can be reused for subsequent invocations.
         // It is initialized when the class is loaded.
-        dynamoDbClient = DependencyFactory.dynamoDbClient();
+        dynamoDbClient = DynamoDbClient.builder()
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .region(Region.US_WEST_1)
+                .httpClientBuilder(UrlConnectionHttpClient.builder())
+                .build();
         // Consider invoking a simple api here to pre-warm up the application, eg: dynamodb#listTables
     }
 
