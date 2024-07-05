9ca1abaa2f35cda6f2dbd346f58053c824888862
jonnie savell <jonniesavell@mac.com>
2024-07-05 14:08:23 -0700

    introduction of billing delete logic

--- src/main/java/com/indigententerprises/App.java
+++ src/main/java/com/indigententerprises/App.java
@@ -1,5 +1,8 @@
 package com.indigententerprises;
 
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.indigententerprises.dto.ApiGatewayResponse;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
@@ -7,31 +10,110 @@ import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsPro
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.ConditionCheck;
+import software.amazon.awssdk.services.dynamodb.model.Delete;
+import software.amazon.awssdk.services.dynamodb.model.TransactWriteItem;
+import software.amazon.awssdk.services.dynamodb.model.TransactWriteItemsRequest;
 
-/**
- * Lambda function entry point. You can change to use other pojo type or implement
- * a different RequestHandler.
- *
- * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java Handler</a> for more information
- */
-public class App implements RequestHandler<Object, Object> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, ApiGatewayResponse> {
 
     private final DynamoDbClient dynamoDbClient;
+    private final String tablename;
 
     public App() {
-        // Initialize the SDK client outside of the handler method so that it can be reused for subsequent invocations.
-        // It is initialized when the class is loaded.
         dynamoDbClient = DynamoDbClient.builder()
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .region(Region.US_WEST_1)
                 .httpClientBuilder(UrlConnectionHttpClient.builder())
                 .build();
-        // Consider invoking a simple api here to pre-warm up the application, eg: dynamodb#listTables
+        tablename = "Organization";
     }
 
     @Override
-    public Object handleRequest(final Object input, final Context context) {
-        // TODO: invoking the api call using dynamoDbClient.
-        return input;
+    public ApiGatewayResponse handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+
+        final Map<String, String> pathParameters = input.getPathParameters();
+        final APIGatewayProxyRequestEvent.RequestIdentity requestIdentity =
+                input.getRequestContext().getIdentity();
+
+        try {
+            final String username = requestIdentity.getUser();
+            final String orgId = pathParameters.get("orgId");
+            final StringBuilder conditionKeyBuilder = new StringBuilder("Admins#");
+            final String conditionStringPk = conditionKeyBuilder.append(orgId).toString();
+            final HashMap<String, AttributeValue> conditionKey = new HashMap<>();
+
+            try {
+                conditionKey.put("PK", AttributeValue.builder().s(conditionStringPk).build());
+
+                final HashMap<String, String> conditionExpressionAttributeNames = new HashMap<>();
+
+                try {
+                    conditionExpressionAttributeNames.put("#admins", "Admins");
+
+                    final HashMap<String, AttributeValue> conditionExpressionAttributeValues = new HashMap<>();
+
+                    try {
+                        final AttributeValue conditionExpressionAttributeValue =
+                                AttributeValue.builder().ss(username).build();
+                        conditionExpressionAttributeValues.put(":user", conditionExpressionAttributeValue);
+
+                        final ConditionCheck conditionCheck = ConditionCheck
+                                .builder()
+                                .tableName(tablename)
+                                .key(conditionKey)
+                                .conditionExpression("contains(#admins, :user)")
+                                .expressionAttributeNames(conditionExpressionAttributeNames)
+                                .expressionAttributeValues(conditionExpressionAttributeValues)
+                                .build();
+                        final TransactWriteItem conditionCheckItem = TransactWriteItem
+                                .builder()
+                                .conditionCheck(conditionCheck)
+                                .build();
+                        final HashMap<String, AttributeValue> deleteKey = new HashMap<>();
+
+                        try {
+                            final StringBuilder deleteKeyBuilder = new StringBuilder("Billing#");
+                            final String deleteStringPk = deleteKeyBuilder.append(orgId).toString();
+                            deleteKey.put("PK", AttributeValue.builder().s(deleteStringPk).build());
+
+                            final Delete delete = Delete
+                                    .builder()
+                                    .key(deleteKey)
+                                    .tableName(tablename)
+                                    .build();
+                            final TransactWriteItem deleteItem = TransactWriteItem.builder()
+                                    .delete(delete)
+                                    .build();
+                            final TransactWriteItemsRequest transactWriteItemsRequest = TransactWriteItemsRequest
+                                    .builder()
+                                    .transactItems(conditionCheckItem, deleteItem)
+                                    .build();
+                            dynamoDbClient.transactWriteItems(transactWriteItemsRequest);
+                        } finally {
+                            deleteKey.clear();
+                        }
+                    } finally {
+                        conditionExpressionAttributeValues.clear();
+                    }
+                } finally {
+                    conditionExpressionAttributeNames.clear();
+                }
+            } finally {
+                conditionKey.clear();
+            }
+
+            final ApiGatewayResponse response = new ApiGatewayResponse(200, "{ \"tableUpdated\": true }");
+            return response;
+        } catch (Exception e) {
+            // TODO: distinguish client fault from server fault
+            // TODO: presumably, a server fault will be retried => this exception corresponds to a client fault.
+            final ApiGatewayResponse response = new ApiGatewayResponse(400, "{ \"tableUpdated\": false }");
+            return response;
+        }
     }
 }
--- src/test/java/com/indigententerprises/AppTest.java
+++ src/test/java/com/indigententerprises/AppTest.java
@@ -1,15 +1,33 @@
 package com.indigententerprises;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
+import com.indigententerprises.dto.ApiGatewayResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class AppTest {
 
     @Test
     public void handleRequest_shouldReturnConstantValue() {
-        App function = new App();
-        Object result = function.handleRequest("echo", null);
-        assertEquals("echo", result);
+        final App function = new App();
+        final Map<String, String> pathParameters = new HashMap<>();
+        pathParameters.put("orgId", UUID.randomUUID().toString());
+        final APIGatewayProxyRequestEvent.RequestIdentity requestIdentity =
+                new APIGatewayProxyRequestEvent.RequestIdentity().withUser("jonniesavell");
+        final APIGatewayProxyRequestEvent.ProxyRequestContext proxyRequestContext =
+                new APIGatewayProxyRequestEvent.ProxyRequestContext().withIdentity(requestIdentity);
+        final APIGatewayProxyRequestEvent apiGatewayEvent = new APIGatewayProxyRequestEvent();
+        apiGatewayEvent.withPathParameters(pathParameters)
+                       .withRequestContext(proxyRequestContext);
+
+        // this lambda does not consult the context
+        final ApiGatewayResponse result = function.handleRequest(apiGatewayEvent, null);
+        assertEquals(400, result.getStatusCode());
     }
 }
